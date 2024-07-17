24017478e64b3d92a0a26281dae4db101575658e
Pavel Goncharov <p.goncharov@centra.ai>
2024-07-17 20:47:04 +0300

    fourth

--- src/modules/newCube/newCube.tsx
+++ src/modules/newCube/newCube.tsx
@@ -1,17 +1,22 @@
 import React, {
+  useEffect,
   //  useEffect, 
   useState } from 'react';
 // import { Square } from '../square/square';
 import './newCube.css'; // Подключение стилей
 
-export const Square = () => {
-  const initialColors = Array.from({ length: 36 }, () => '#ffffff'); // Создаем массив из 36 белых цветов
+export const Square = ({setSides}: {setSides: any}) => {
+  const initialColors = Array.from({ length: 36 }, () => 'red'); // Создаем массив из 36 белых цветов
   const [colors, setColors] = useState(initialColors);
 
   const handleClick = (index: number) => {
     const newColors = [...colors];
-    newColors[index] = '#' + Math.floor(Math.random() * 16777215).toString(16); // Генерируем случайный цвет
+    newColors[index] = 'green'; // Генерируем случайный цвет
     setColors(newColors);
+    if (newColors.every(color => color === 'green')) {
+      setSides((prev: any) => [...prev, 'side'])
+    }
+
   };
 
   return (
@@ -29,7 +34,89 @@ export const Square = () => {
 
 }
 
-export const NewCube = () => {
+export const SquareSmall = () => {
+  const initialColors = Array.from({ length: 16 }, () => '#ffffff'); // Создаем массив из 36 белых цветов
+  const [colors, setColors] = useState(initialColors);
+
+  const handleClick = (index: number) => {
+    const newColors = [...colors];
+    newColors[index] = '#' + Math.floor(Math.random() * 16777215).toString(16); // Генерируем случайный цвет
+    setColors(newColors);
+  };
+
+  return (
+    <div className="square2">
+      {colors.map((color: string, index: number) => (
+        <div
+          key={index}
+          className="little-square2"
+          style={{ backgroundColor: color }}
+          onClick={() => handleClick(index)}
+        ></div>
+      ))}
+    </div>
+  );
+
+}
+
+export const NewCube = ({setChange}: {setChange: any}) => {
+  const [sides, setSides] = useState([])
+  useEffect(() => {
+    if(sides.length === 6) {
+      setChange(true)
+    }
+  }, [setChange, sides.length])
+  let x = 0,
+    y = 0
+  const handleUp = () => {
+      x += 5
+      const a = document?.querySelector('.cube') as HTMLDivElement
+      if (a) {
+        a.style.transform = `rotateY(${y}deg) rotateX(${x}deg)`
+      }
+  }
+  const handleDown = () => {
+      x -= 5
+      const a = document?.querySelector('.cube') as HTMLDivElement
+      if (a) {
+        a.style.transform = `rotateY(${y}deg) rotateX(${x}deg)`
+      }
+  }
+  const handleRight = () => {
+    y -= 5
+    const a = document?.querySelector('.cube') as HTMLDivElement
+    if (a) {
+      a.style.transform = `rotateY(${y}deg) rotateX(${x}deg)`
+    }
+}
+const handleLeft = () => {
+  y += 5
+  const a = document?.querySelector('.cube') as HTMLDivElement
+  if (a) {
+    a.style.transform = `rotateY(${y}deg) rotateX(${x}deg)`
+  }
+}
+  return (
+    <div>
+      <div style={{ marginBottom: '20px', position: 'absolute', top: 0, gap: 16 }}>
+        <button onClick={handleUp}>up</button>
+        <button onClick={handleLeft}>left</button>
+        <button onClick={handleDown}>down</button>
+        <button onClick={handleRight}>right</button>
+      </div>
+      <div className="cube">
+        <div className="side front"><Square setSides={setSides}/></div>
+        <div className="side back"><Square setSides={setSides}/></div>
+        <div className="side left"><Square setSides={setSides}/></div>
+        <div className="side right"><Square setSides={setSides}/></div>
+        <div className="side top"><Square setSides={setSides}/></div>
+        <div className="side bottom"><Square setSides={setSides}/></div>
+      </div>
+    </div>
+  );
+}
+
+export const NewCubeSmall = () => {
   let x = 0,
     y = 0
   const handleUp = () => {
@@ -55,26 +142,26 @@ export const NewCube = () => {
 }
 const handleLeft = () => {
   y -= 5
-  const a = document?.querySelector('.cube') as HTMLDivElement
+  const a = document?.querySelector('.cube2') as HTMLDivElement
   if (a) {
     a.style.transform = `rotateY(${y}deg) rotateX(${x}deg)`
   }
 }
   return (
     <div>
-      <div style={{ marginBottom: '20px', position: 'absolute', top: 0 }}>
+      <div style={{ marginBottom: '20px', position: 'absolute', top: 0, justifyContent: 'center', alignItems: 'center' }}>
         <button onClick={handleUp}>up</button>
         <button onClick={handleLeft}>left</button>
         <button onClick={handleDown}>down</button>
         <button onClick={handleRight}>right</button>
       </div>
-      <div className="cube">
-        <div className="side front"><Square /></div>
-        <div className="side back"><Square /></div>
-        <div className="side left"><Square /></div>
-        <div className="side right"><Square /></div>
-        <div className="side top"><Square /></div>
-        <div className="side bottom"><Square /></div>
+      <div className="cube2">
+      <div className="side2 front2"><SquareSmall /></div>
+        <div className="side2 back2"><SquareSmall /></div>
+        <div className="side2 left2"><SquareSmall /></div>
+        <div className="side2 right2"><SquareSmall /></div>
+        <div className="side2 top2"><SquareSmall /></div>
+        <div className="side2 bottom2"><SquareSmall /></div>
       </div>
     </div>
   );
--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
+import  {
+    //  useEffect, 
+    useState } from 'react'
 // import { Square } from "../modules/square/square";
 // import { Cube } from '../modules/cube/cube';
-import { NewCube } from '../modules/newCube/newCube';
+import { NewCube, NewCubeSmall } from '../modules/newCube/newCube';
 // import { ThreeDCube } from '../modules/cube/cube';
 
 export const MainPage = () => {
+    const [change, setChange] = useState(false)
     return (
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{display: 'flex', flexDirection: 'column', gap: 16, alignContent: 'center', justifyContent: 'center'}}>
             {/* <Square /> */}
             {/* <Cube /> */}
-            <NewCube />
+            {!change ? <NewCube setChange={setChange}/> :
+            <NewCubeSmall />}
         </div>
     )
 }
