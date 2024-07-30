6f7e19bd54032bce08a8c570105c2b407d0f55db
Pavel Goncharov <p.goncharov@centra.ai>
2024-07-30 19:40:08 +0300

    500

--- src/modules/disapear/disapear.tsx
+++ src/modules/disapear/disapear.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import './disapear.css'
 
 export const Dissapear = () => {
     // const [largeSquares, setLargeSquares] = useState(Array(36).fill(true));
-    const [smallSquaresOpacity, setSmallSquaresOpacity] = useState(Array(122500).fill(1));
+    const [smallSquaresOpacity, setSmallSquaresOpacity] = useState(Array(250000).fill(1));
     // const [smallSquaresOpacity2, setSmallSquaresOpacity2] = useState(Array(16).fill(1));
     const [zoom, setZoom] = useState(0.5);
 
@@ -16,13 +16,13 @@ export const Dissapear = () => {
     };
   
   
-    const handleSmallSquareClick = (index: any) => {
-        setSmallSquaresOpacity(prevOpacity => {
-            const newOpacity = [...prevOpacity];
-            newOpacity[index] = 0;
-            return newOpacity;
-        });
-    };
+    const handleSmallSquareClick = useCallback((index: any) => {
+      setSmallSquaresOpacity(prevOpacity => {
+          const newOpacity = [...prevOpacity];
+          newOpacity[index] = 0;
+          return newOpacity;
+      });
+  }, [setSmallSquaresOpacity]);
 
     // const handleSmallSquareClick2 = (index: any) => {
     //     setSmallSquaresOpacity2(prevOpacity => {
@@ -39,7 +39,7 @@ export const Dissapear = () => {
         <button onClick={handleZoomIn}>+</button>
         <button onClick={handleZoomOut}>-</button>
         <div style={{position: 'relative', overflow: 'hidden'}}>
-        {noBigSquare && <div className="large-square" style={{ order: 1, transform: `scale(${zoom})`, transformOrigin: 'center', transition: 'transform 0.2s' }}>
+        {noBigSquare && <div className="large-square" style={{ order: 1, transform: `scale(${zoom})`, transformOrigin: 'center' }}>
           {smallSquaresOpacity.map((opacity, index) => (
             <div key={index} className={`small-square`} style={{ opacity: opacity }} onClick={() => handleSmallSquareClick(index)} />
           ))}
